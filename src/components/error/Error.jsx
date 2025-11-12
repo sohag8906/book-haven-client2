1e995ce117942f86aab69486ee291c746eaee647
Your Namesohaggaji07@gmail.com <sohaggaji07@gmail.com>
2025-11-12 13:58:22 +0600

    error

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import errorImg from '../../assets/error.webp';
 
 const Error = () => {
-    return (
-        <div className="text-center mt-20">
+  return (
+    <div className="flex flex-col justify-center items-center mt-5 text-center">
+      <img
+        className="w-[400px] h-[300px] mb-4"
+        src={errorImg}
+        alt="Error Illustration"
+      />
       <h1 className="text-5xl font-bold text-red-600">404</h1>
-      <p className="text-xl mt-4">Page Not Found 😢</p>
+      <p className="text-xl mt-2 text-gray-600">Page Not Found !!</p>
     </div>
-    );
+  );
 };
 
 export default Error;
